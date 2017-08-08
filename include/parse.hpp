437e41ac72cf4e4f82d4d8c035041908f9a374fb
Sam <soc27@humboldt.edu>
2017-08-08 12:18:45 -0700

    Add forward declarations to header

--- include/parse.hpp
+++ include/parse.hpp
@@ -12,6 +12,8 @@
 bool get_comment(std::ifstream &file);
 std::optional<double> get_number(std::ifstream &file, char end_char);
 std::optional<std::string> get_name(std::ifstream &file, bool paren_started = false);
+std::optional<std::string> get_string(std::ifstream &file);
+std::optional<CommandList> get_commands(std::ifstream &file);
 std::optional<std::pair<std::string, CommandList>> get_func(std::ifstream &file);
 std::optional<std::pair<std::string, Class>> get_class(std::ifstream &file);
 std::optional<std::map<std::string, Class>> get_classes(std::ifstream &file);
